f7fa7a4e5f9588050da0a1ec764d3cb935eeaa30
Melfiah ts <melfiahts@gmail.com>
2019-12-03 22:44:24 -0500

    changes made

--- actiTime_proj2/src/test/java/com/actiTime/step_defs/Blog_steps.java
+++ actiTime_proj2/src/test/java/com/actiTime/step_defs/Blog_steps.java
@@ -20,7 +20,7 @@ public class Blog_steps {
 	@Given("Iam on the actiTime home page")
 	public void iam_on_the_actiTime_home_page() {
 		System.setProperty("webdriver.chrome.driver",
-				"C:/Users/melfi/Desktop/Selenium drivers/chromedriver_win32/chromedriver.exe/");
+				System.getProperty("user.dir")+"/drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.get("https://www.actitime.com");
